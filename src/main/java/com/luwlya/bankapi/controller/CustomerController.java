59666e2efb9c0711fc08d892ff51f151624a4f09
Lu Kotova <loui.kotova@gmail.com>
2023-08-21 23:30:02 +0200

    add customer services

--- src/main/java/com/luwlya/bankapi/controller/CustomerController.java
+++ src/main/java/com/luwlya/bankapi/controller/CustomerController.java
@@ -8,9 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.OffsetDateTime;
-import java.util.List;
-
 @RestController
 public class CustomerController {
     private CustomerService customerService;
