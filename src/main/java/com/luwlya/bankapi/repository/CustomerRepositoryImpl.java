62224e57314c8b19f564caa853d54c227b69bf86
Lu Kotova <loui.kotova@gmail.com>
2023-09-02 22:52:33 +0200

    implement Customer Repository via JDBC

--- src/main/java/com/luwlya/bankapi/repository/CustomerRepositoryImpl.java
+++ src/main/java/com/luwlya/bankapi/repository/CustomerRepositoryImpl.java
@@ -5,19 +5,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import javax.sql.DataSource;
+import java.sql.*;
 import java.time.OffsetDateTime;
 import java.util.List;
 import java.util.UUID;
 
 @Repository
 public class CustomerRepositoryImpl implements CustomerRepository {
-    private JdbcTemplate jdbcTemplate;
+    private DataSource dataSource;
 
     @Autowired
-    public CustomerRepositoryImpl(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
+    public CustomerRepositoryImpl(DataSource dataSource) {
+        this.dataSource = dataSource;
     }
 
     private static Customer extractCustomer(ResultSet rs) throws SQLException {
@@ -34,24 +34,36 @@ public class CustomerRepositoryImpl implements CustomerRepository {
 
     @Override
     public void insert(Customer customer) {
-        jdbcTemplate.update("insert into customers(id,first_name,last_name,email,address,phone,created_at,updated_at) " +
-                        "values (?,?,?,?,?,?,?,?)",
-                customer.id(),
-                customer.firstName(),
-                customer.lastName(),
-                customer.email(),
-                customer.address(),
-                customer.phone(),
-                customer.createdAt(),
-                customer.updatedAt());
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(
+                     "INSERT INTO customers(id,first_name,last_name,email,address,phone,created_at,updated_at) " +
+                             "VALUES (?,?,?,?,?,?,?,?)");
+        ) {
+            statement.setObject(1, customer.id());
+            statement.setString(2, customer.firstName());
+            statement.setString(3, customer.lastName());
+            statement.setString(4, customer.email());
+            statement.setString(5, customer.address());
+            statement.setString(6, customer.phone());
+            statement.setObject(7, customer.createdAt());
+            statement.setObject(8, customer.updatedAt());
+            statement.execute();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public Customer get(UUID id) {
-        return jdbcTemplate.query("SELECT * FROM customers WHERE id = ?",
-                CustomerRepositoryImpl::extractCustomer,
-                id
-        );
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement("SELECT * FROM customers WHERE id = ?");
+        ) {
+            statement.setObject(1, id);
+            ResultSet resultSet = statement.executeQuery();
+            return extractCustomer(resultSet);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
