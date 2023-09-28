9dd8098c8a9242648cc1e4ea0d5b9f525a4c0416
Lu Kotova <loui.kotova@gmail.com>
2023-09-29 00:21:43 +0200

    refactor customer repository to use jdbc template

--- src/main/java/com/luwlya/bankapi/repository/impl/CustomerRepositoryImpl.java
+++ src/main/java/com/luwlya/bankapi/repository/impl/CustomerRepositoryImpl.java
@@ -5,29 +5,26 @@ import com.luwlya.bankapi.model.Customer;
 import com.luwlya.bankapi.model.CustomerStatus;
 import com.luwlya.bankapi.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.datasource.DataSourceUtils;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
-import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.OffsetDateTime;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
 @Repository
 public class CustomerRepositoryImpl implements CustomerRepository {
-    private DataSource dataSource;
+    private JdbcTemplate jdbcTemplate;
 
     @Autowired
-    public CustomerRepositoryImpl(DataSource dataSource) {
-        this.dataSource = dataSource;
+    public CustomerRepositoryImpl(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
     }
 
-    private static Customer extractCustomer(ResultSet rs) throws SQLException {
+    private Customer extractCustomer(ResultSet rs, int rowNum) throws SQLException {
         return new Customer(rs.getObject("id", UUID.class),
                 CustomerStatus.valueOf(rs.getString("status")),
                 rs.getString("first_name"),
@@ -41,97 +38,57 @@ public class CustomerRepositoryImpl implements CustomerRepository {
 
     @Override
     public void insert(Customer customer) throws SQLException {
-        try (Connection connection = getConnection();
-             PreparedStatement statement = connection.prepareStatement(
-                     "INSERT INTO customers(id,status, first_name,last_name,email,address,phone,created_at,updated_at) " +
-                             "VALUES (?,?,?,?,?,?,?,?,?)");
-        ) {
-            statement.setObject(1, customer.id());
-            statement.setString(2, customer.status().name());
-            statement.setString(3, customer.firstName());
-            statement.setString(4, customer.lastName());
-            statement.setString(5, customer.email());
-            statement.setString(6, customer.address());
-            statement.setString(7, customer.phone());
-            statement.setObject(8, customer.createdAt());
-            statement.setObject(9, customer.updatedAt());
-            statement.execute();
-        }
+        jdbcTemplate.update(
+                "INSERT INTO customers(id,status, first_name,last_name,email,address,phone,created_at,updated_at) " +
+                        "VALUES (?,?,?,?,?,?,?,?,?)",
+                customer.id(),
+                customer.status().name(),
+                customer.firstName(),
+                customer.lastName(),
+                customer.email(),
+                customer.address(),
+                customer.phone(),
+                customer.createdAt(),
+                customer.updatedAt()
+        );
     }
 
     @Override
     public Customer get(UUID id) {
-        try (Connection connection = getConnection();
-             PreparedStatement statement = connection.prepareStatement("SELECT * FROM customers WHERE id = ?");
-        ) {
-            statement.setObject(1, id);
-            ResultSet resultSet = statement.executeQuery();
-            if (!resultSet.next()) {
-                throw new CustomerNotFoundException(id);
-            }
-            return extractCustomer(resultSet);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
+        try {
+            return jdbcTemplate.queryForObject(
+                    "SELECT * FROM customers WHERE id = ?",
+                    this::extractCustomer,
+                    id
+            );
+        } catch (EmptyResultDataAccessException e) {
+            throw new CustomerNotFoundException(id);
         }
     }
 
     @Override
     public List<Customer> getAll() {
-        try (Connection connection = getConnection();
-             PreparedStatement statement = connection.prepareStatement("SELECT * FROM customers");
-        ) {
-            List<Customer> customers = new ArrayList<>();
-            ResultSet resultSet = statement.executeQuery();
-            while (resultSet.next()) {
-                customers.add(extractCustomer(resultSet));
-            }
-            return customers;
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+        return jdbcTemplate.query("SELECT * FROM customers", this::extractCustomer);
     }
 
     @Override
     public void update(Customer customer) {
-        try (Connection connection = getConnection();
-             PreparedStatement statement = connection.prepareStatement("UPDATE customers " +
-                     "SET first_name = ?, " +
-                     "last_name = ?," +
-                     "email = ?," +
-                     "address =?," +
-                     "phone = ?," +
-                     "updated_at = ?" +
-                     " WHERE id=?");
-        ) {
-            statement.setObject(1, customer.firstName());
-            statement.setObject(2, customer.lastName());
-            statement.setObject(3, customer.email());
-            statement.setObject(4, customer.address());
-            statement.setObject(5, customer.phone());
-            statement.setObject(6, customer.updatedAt());
-            statement.setObject(7, customer.id());
-            statement.execute();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+        jdbcTemplate.update(
+                "UPDATE customers SET first_name = ?, last_name = ?,email = ?,address =?,phone = ?,updated_at = ? WHERE id=?",
+                customer.firstName(),
+                customer.lastName(),
+                customer.email(),
+                customer.address(),
+                customer.phone(),
+                customer.updatedAt(),
+                customer.id()
+        );
     }
 
     @Override
     public boolean delete(UUID id) {
-        try (Connection connection = getConnection();
-             PreparedStatement statement = connection.prepareStatement("UPDATE customers SET status = ? WHERE id=? AND status = ?");
-        ) {
-            statement.setString(1, CustomerStatus.INACTIVE.name());
-            statement.setObject(2, id);
-            statement.setString(3, CustomerStatus.ACTIVE.name());
-            statement.execute();
-            return statement.getUpdateCount() > 0;
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private Connection getConnection() throws SQLException {
-        return DataSourceUtils.getConnection(dataSource);
+        int updated = jdbcTemplate.update("UPDATE customers SET status = ? WHERE id=? AND status = ?",
+                CustomerStatus.INACTIVE.name(), id, CustomerStatus.ACTIVE.name());
+        return updated > 0;
     }
 }
